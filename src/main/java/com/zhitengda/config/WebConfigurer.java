7858bd43c7eaf2cd4b6b13ff4ba8760b026b0e72
weixiaojian <2916863213@qq.com>
2020-07-31 16:38:07 +0800

    提交：跨域问题处理

---
+++ src/main/java/com/zhitengda/config/WebConfigurer.java
@@ -0,0 +1,53 @@
+package com.zhitengda.config;
+
+import com.zhitengda.interceptor.WxAuthInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * web配置类
+ *
+ * @author langao_q
+ * @since 2020-07-29 17:33
+ */
+@Configuration
+public class WebConfigurer implements WebMvcConfigurer {
+
+    @Autowired
+    private WxAuthInterceptor wxAuthInterceptor;
+
+    /**
+     * 配置微信授权拦截器
+     *
+     * @param registry
+     */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        // addPathPatterns("/**") 表示拦截所有的请求，
+        // excludePathPatterns("/login", "/register") 表示除了登陆与注册之外，因为登陆注册不需要登陆也可以访问
+        registry.addInterceptor(wxAuthInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/error/*", "/msg/*", "/user/*");
+    }
+
+    /**
+     * 前后端分离：跨域问题解决
+     * @param registry
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // 所有的当前站点的请求地址，都支持跨域访问。
+        registry.addMapping("/**")
+                // 当前站点支持的跨域请求类型是什么。
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                // 是否支持跨域用户凭证
+                .allowCredentials(true)
+                // 所有的外部域都可跨域访问。 如果是localhost则很难配置，因为在跨域请求的时候，外部域的解析可能是localhost、127.0.0.1、主机名
+                .allowedOrigins("*")
+                // 超时时长设置为1小时。 时间单位是秒。
+                .maxAge(60);
+    }
+}
